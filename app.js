bedf9454fadb7fd985a957cc59637bbbb0194d46
Paula Suzart <121134832+paulinhadev2@users.noreply.github.com>
2024-01-25 03:23:07 -0300

    Update app.js
    o math.random antigamente estava fazendo o código cair no numero menor se colocássemos o número 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroSecreto = parseInt("100");
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
